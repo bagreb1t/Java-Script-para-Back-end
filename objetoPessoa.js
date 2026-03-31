4cef7bd79f9fcba037f2f1002e9601d321e73dab
ROBERSON FERREIRA FARIAS <167565670+bagreb1t@users.noreply.github.com>
2026-03-31 18:00:16 -0300

    Criação de objetos
    Acessamos os objetos por "." ou [ ]

--- objetoPessoa.js
+++ objetoPessoa.js
@@ -1,11 +1,24 @@
-// let pessoa=['Ana', 20, 'Estudante', 'paranaense']
-
-pessoa={
+let clientes=[   
+pessoa1={
 nome:'Ana',
 idade:20,
-ocupação:'Estudante',
-naturalidade:'paranaense'
- 
-}
-
-console.log( pessoa   )
+ocupação:'estudante',
+naturalidade:'paraná',
+casada:true,
+endereço:{
+    rua:'Marechal Floriano',
+    número:1254,
+    bairro:'Hauer'
+}}, pessoa2={
+nome:'Jorge',
+idade:20,
+ocupação:'estudante',
+naturalidade:'paraná',
+casada:true,
+endereço:{
+    rua:'Marechal Floriano',
+    número:1254,
+    bairro:'Hauer'
+}}
+]
+console.log(`O nome da rua é ${pessoa1.endereço.rua}`)
